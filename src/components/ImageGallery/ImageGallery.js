0e8c748247b69f605a6b074f89f7404c87cb5344
Nazarenko Alex <nazarenko.alx@gmail.com>
2023-03-12 17:13:03 +0200

    hooked imagegallery

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,32 +1,28 @@
-import { Component } from 'react';
 import { GalleryList } from './ImageGallery.styled';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import PropTypes from 'prop-types';
 
-export class ImageGallery extends Component {
-  render() {
-    const { pictures } = this.props;
-    return (
-      <>
-        {Boolean(pictures.length) && (
-          <GalleryList>
-            {pictures.map(picture => {
-              const { id, webformatURL, largeImageURL, tags } = picture;
-              return (
-                <ImageGalleryItem
-                  key={id}
-                  webformatURL={webformatURL}
-                  largeImageURL={largeImageURL}
-                  tags={tags}
-                />
-              );
-            })}
-          </GalleryList>
-        )}
-      </>
-    );
-  }
-}
+export const ImageGallery = ({ pictures }) => {
+  return (
+    <>
+      {Boolean(pictures.length) && (
+        <GalleryList>
+          {pictures.map(picture => {
+            const { id, webformatURL, largeImageURL, tags } = picture;
+            return (
+              <ImageGalleryItem
+                key={id}
+                webformatURL={webformatURL}
+                largeImageURL={largeImageURL}
+                tags={tags}
+              />
+            );
+          })}
+        </GalleryList>
+      )}
+    </>
+  );
+};
 
 ImageGallery.propTypes = {
   pictures: PropTypes.arrayOf(
