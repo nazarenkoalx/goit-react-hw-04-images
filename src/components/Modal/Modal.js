d78083372b3bf3ff27d857769a3ee3734f12c835
Nazarenko Alex <nazarenko.alx@gmail.com>
2023-03-12 17:51:46 +0200

    hooked modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,40 +1,38 @@
 import { Overlay, ModalWindow } from './Modal.styled';
 import { createPortal } from 'react-dom';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount = () => {
-    window.addEventListener('keydown', this.onKeydown);
-  };
+export const Modal = ({ children, onModalShow }) => {
+  useEffect(() => {
+    const onKeydown = evt => {
+      if (evt.code === 'Escape') {
+        onModalShow();
+      }
+    };
 
-  componentWillUnmount = () => {
-    window.removeEventListener('keydown', this.onKeydown);
-  };
+    document.addEventListener('keydown', onKeydown);
 
-  onKeydown = evt => {
-    if (evt.code === 'Escape') {
-      this.props.onModalShow();
-    }
-  };
+    return () => {
+      document.removeEventListener('keydown', onKeydown);
+    };
+  }, [onModalShow]);
 
-  onBackdropClick = evt => {
+  const onBackdropClick = evt => {
     if (evt.target === evt.currentTarget) {
-      this.props.onModalShow();
+      onModalShow();
     }
   };
 
-  render() {
-    return createPortal(
-      <Overlay onClick={this.onBackdropClick}>
-        <ModalWindow>{this.props.children}</ModalWindow>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <Overlay onClick={onBackdropClick}>
+      <ModalWindow>{children}</ModalWindow>
+    </Overlay>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
   onModalShow: PropTypes.func.isRequired,
